c4f6ddc5527f7d6c79a64109014c82c1f842f7bf
James Stevenson <james.stevenson@nationwidechildrens.org>
2024-11-22 15:22:08 -0500

    feat: add additioanl vote metadata

--- script.js
+++ script.js
@@ -25,29 +25,28 @@ const addVoteFeatures = () => {
 		loadJson("./assets/2024_general_issue_47_votes.json").then((votes) => {
 			const voteTable = {};
 			votes.forEach((vote) => {
-				voteTable[vote.precinct_name] = vote.yes_47 / (vote.yes_47 + vote.no_47 + vote.blank_47);
+				voteTable[vote.precinct_name] =
+          {
+            totalVotes: vote.yes_47 + vote.no_47 + vote.blank_47,
+            yesVotes: vote.yes_47,
+          }
 			});
-
-			const values = Object.values(voteTable);
-			const maxVotes = Math.max(...values);
       const thresholds = [0.0, 0.125, 0.25, 0.375, 0.5, 0.625, 0.75, 0.875];
 
-      console.log(thresholds);
-
-			const colorByOctile = (count) => {
-				return count > thresholds[6]
+			const colorByOctile = (num) => {
+				return num > thresholds[6]
 					? ABS_COLORS[0]
-					: count > thresholds[5]
+					: num > thresholds[5]
 					? ABS_COLORS[1]
-					: count > thresholds[4]
+					: num > thresholds[4]
 					? ABS_COLORS[2]
-					: count > thresholds[3]
+					: num > thresholds[3]
 					? ABS_COLORS[3]
-					: count > thresholds[2]
+					: num > thresholds[2]
 					? ABS_COLORS[4]
-					: count > thresholds[1]
+					: num > thresholds[1]
 					? ABS_COLORS[5]
-					: count > thresholds[0]
+					: num > thresholds[0]
 					? ABS_COLORS[6]
 					: ABS_COLORS[7];
 			};
@@ -55,22 +54,26 @@ const addVoteFeatures = () => {
 			L.geoJSON(precinctShapes, {
 				style: (feature) => {
 					const precinctId = feature.properties.NAME;
-					const votes = voteTable[precinctId] || 0;
+          const votes = voteTable[precinctId];
+          const ratio = votes.yesVotes / votes.totalVotes || 0;
 					return {
 						color: "#3388FF",
 						weight: 1,
-						fillColor: colorByOctile(votes),
+						fillColor: colorByOctile(ratio),
 						fillOpacity: 0.6,
 					};
 				},
 				onEachFeature: (feature, layer) => {
 					if (feature.properties) {
 						const { NAME, PRECINCTID } = feature.properties;
-						const votes = voteTable[NAME] || 0;
+						const votes = voteTable[NAME];
+            const ratio = votes.yesVotes / votes.totalVotes || 0;
 						layer.bindPopup(`
             <b>Precinct id:</b> ${PRECINCTID}<br>
             <b>Name:</b> ${NAME}<br>
-            <b>Votes:</b> ${votes}
+            <b>Yes Pct:</b> ${(ratio * 100).toFixed(2)}%<br>
+            <b>Yes Votes:</b> ${votes.yesVotes}<br>
+            <b>Total Votes:</b> ${votes.totalVotes}
           `);
 					}
 				},
